f3a75d8bf74ae8b0858958c313a6f79fc53a5775
AndyChiangSH <andy10801@gmail.com>
2025-06-05 16:07:04 +0800

    Lab 4: run.sh

--- Lab/Lab_4/Qualcomm AI Hub/test.sh
+++ Lab/Lab_4/Qualcomm AI Hub/test.sh
@@ -1,3 +1,5 @@
-# python -m qai_hub_models.models.llama_v3_2_3b_instruct.export --target-runtime qnn_context_binary --device sm8550
-python -m qai_hub_models.models.llama_v3_2_3b_instruct.demo --host-device sm8550
+# python -m qai_hub_models.models.ffnet_40s.demo
 
+# python -m qai_hub_models.models.ffnet_40s.export
+
+# python -m qai_hub_models.models.ffnet_40s.demo --on-device
